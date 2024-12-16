910d50df880b7d25c246df9a64ce6d9a77e05f69
guillermoVicenteGonzalez <guillermovg@usal.es>
2024-12-16 10:28:00 +0100

    feat(husky): last husky configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
-import "./App.css";
 import { Button } from "../";
+import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<Button></Button>
