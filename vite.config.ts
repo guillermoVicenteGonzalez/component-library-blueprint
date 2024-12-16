5e53d4c3a98a8e07918d2eddc972db7309fecfad
guillermoVicenteGonzalez <guillermovg@usal.es>
2024-12-16 10:36:38 +0100

    feat(husky): now husky works

--- vite.config.ts
+++ vite.config.ts
@@ -8,54 +8,55 @@ import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    libInjectCss(),
-    dts({
-      include: ["lib"],
-      exclude: [
-        "lib/**/*.stories.tsx",
-        "**/*.stories.tsx",
-        "**/*.stories.*",
-        "lib/**/stories.*",
-        "**/*.stories.d.ts",
-        "lib/**/*.stories.d.ts",
-      ],
-    }),
-  ],
-  build: {
-    emptyOutDir: false,
-    lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["react", "react/jsx-runtime"],
-      input: Object.fromEntries(
-        glob
-          .sync("lib/**/*.{ts,tsx}", {
-            ignore: [
-              "lib/**/*.d.ts",
-              "lib/**/*.stories.tsx",
-              "**/*.stories.d.ts",
-              "**/*.stories.d.*",
-              "**/*.stories.d.ts",
-              "lib/**/*.stories.d.ts",
-            ],
-          })
-          .map((file) => [
-            // The name of the entry point
-            // lib/nested/foo.ts becomes nested/foo
-            relative("lib", file.slice(0, file.length - extname(file).length)),
-            // The absolute path to the entry file
-            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
-      output: {
-        assetFileNames: "assets/[name][extname]",
-        entryFileNames: "[name].js",
-      },
-    },
-  },
+	plugins: [
+		react(),
+		libInjectCss(),
+		dts({
+			include: ["lib"],
+			exclude: [
+				"lib/**/*.stories.tsx",
+				"**/*.stories.tsx",
+				"**/*.stories.*",
+				"lib/**/stories.*",
+				"**/*.stories.d.ts",
+				"lib/**/*.stories.d.ts",
+				"/commitlint.config.ts",
+			],
+		}),
+	],
+	build: {
+		emptyOutDir: false,
+		lib: {
+			entry: resolve(__dirname, "lib/main.ts"),
+			formats: ["es"],
+		},
+		rollupOptions: {
+			external: ["react", "react/jsx-runtime"],
+			input: Object.fromEntries(
+				glob
+					.sync("lib/**/*.{ts,tsx}", {
+						ignore: [
+							"lib/**/*.d.ts",
+							"lib/**/*.stories.tsx",
+							"**/*.stories.d.ts",
+							"**/*.stories.d.*",
+							"**/*.stories.d.ts",
+							"lib/**/*.stories.d.ts",
+						],
+					})
+					.map(file => [
+						// The name of the entry point
+						// lib/nested/foo.ts becomes nested/foo
+						relative("lib", file.slice(0, file.length - extname(file).length)),
+						// The absolute path to the entry file
+						// lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+						fileURLToPath(new URL(file, import.meta.url)),
+					])
+			),
+			output: {
+				assetFileNames: "assets/[name][extname]",
+				entryFileNames: "[name].js",
+			},
+		},
+	},
 });
